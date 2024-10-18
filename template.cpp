316b30dea46e2ed1278c580e1ff377999c15a61d
Ryan Fu <ryanfu@Ryans-MacBook-Air-2.local>
2024-10-17 18:05:13 -0700

    template, folders

---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+signed main() {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	
+	return 0;
+}
